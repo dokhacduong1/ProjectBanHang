365b8ffc483f9b036d5ae8a85d8893c2fc897842
Do Khac Duong <90135701+dokhacduong1@users.noreply.github.com>
2023-04-05 01:58:16 +0700

    updatev4

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SectionTwo from './components/SectionTwo';
 import SectionThree from './components/SectionThree';
 import SectionFour from './components/SectionFour';
 import SectionFive from './components/SectionFive';
+import SectionSix from './components/SectionSix';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <SectionThree/>
       <SectionFour/>
       <SectionFive/>
+      <SectionSix/>
    </>
   );
 }
---
+++ src/components/SectionSix/index.js
@@ -0,0 +1,28 @@
+import "./SectionSix.css"
+function SectionSix(){
+    return(
+        <>
+            <section className="SectionSix">
+                <div className="container">
+                <div class="SectionSix__boxIcon">
+                    <div class="SectionSix__boxIcon--list">
+                        <ul class="social SectionSix__boxUl">
+                            <li><a href=" "><i class="fa-brands fa-facebook"></i></a></li>
+                            <li><a href=" "><i class="fa-brands fa-twitter"></i></a></li>
+                            <li><a href=" "><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li><a href=" "><i class="fa-solid fa-rss"></i></a></li>
+                            <li><a href=" "><i class="fa-brands fa-dribbble"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 text-center ">
+                        <p class="SectionSix__endContent">Thank You For Visiting Our Website.<br/> We Hope You Have Found Useful And Interesting Information.</p>
+                    </div>
+                </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default SectionSix
